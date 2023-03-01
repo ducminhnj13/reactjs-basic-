61ec4feef8c3383608d0e4ef5d26a6cda9f4dbd9
Duc Minh <buiducminhnj13@gmail.com>
2023-03-01 22:18:17 +0700

    update

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MyComponent from './example/MyComponents';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello tat ca cac nguoi anh em thien lanh
         </p>
         <a
           className="App-link"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent/>  
       </header>
     </div>
   );
---
+++ src/views/example/MyComponents.js
@@ -0,0 +1,46 @@
+import React from 'react';
+class MyComponent extends React.Component{
+    /*
+    JSX chi return ve 1 block muon tao nhieu div, thì bọc tất cả các div bên trong 1 div khác
+    */
+
+    state = {
+        name:'minh1',
+        address:'ha noi'
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnClickButton = () => {
+        alert('click me')
+    }
+
+    render() {
+        // xem sự thay đổi của state mỗi khi thay đổi giá trị trong ô input
+        // console.log('check render state:',this.state)
+        return (
+            <>
+                <div className='first'>
+                <input value={this.state.name} type="text"
+                    onChange={(event)=> this.handleOnChangeName(event)}
+                >
+                </input>
+                xin chao anh em 
+                day la JSX, ten toi la {this.state.name}
+                </div>
+                <div className='second'>
+                my address: {this.state.address}
+                </div>
+                {/* DOM event */}
+                <div className='thirst'>
+                    <button onClick={()=>this.handleOnClickButton()} >Click me!</button>
+                </div>
+            </>
+            )
+    }
+}
+export default MyComponent;
