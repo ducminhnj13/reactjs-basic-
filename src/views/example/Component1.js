7eb692634d89f511607576b6bd149cc3748482fb
Duc Minh <buiducminhnj13@gmail.com>
2023-03-03 22:36:41 +0700

    Conditional Output

--- src/views/example/Component1.js
+++ src/views/example/Component1.js
@@ -1,53 +1,68 @@
 import React from "react";
 
 // class component
-// class Component1 extends React.Component {
-//   render() {
-//     console.log("checks props", this.props);
+class Component1 extends React.Component {
+  state = {
+    showJobs: false,
+  };
 
-//     // cach 1
-//     // let name = this.props.name;
-//     // let age = this.props.age;
+  handleShowHide = (status) => {
+    this.setState({ showJobs: !this.state.showJobs });
+  };
+  render() {
+    let { arrJobs } = this.props;
+    let { showJobs } = this.state;
+    let check = showJobs === true ? "showJobs = true" : "showJobs = false";
+    console.log("conditional check:", check);
+    return (
+      <>
+        {showJobs === false ?
+          <div>
+            <button onClick={() => {this.handleShowHide();}}>Show</button>
+          </div>
+        
+        :
+          <>
+            <div className="job-lists">
+              {arrJobs.map((item, index) => {
+                return (
+                  <div key={item.id}>
+                    {item.title} - {item.price}
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              <button onClick={() => {this.handleShowHide();}}>Hide</button>
+            </div>
+          </>
+        }
+      </>
+    );
+  }
+}
 
-//     // cach 2
-//     let { name, age, address, arrJobs } = this.props;
+// -************************-
+// function component
+// const Component1 = (props) => {
+
+//     let { arrJobs } = props;
 //     return (
 //       <>
 //         <div className="job-lists">
 //           {arrJobs.map((item, index) => {
-//             return (
+//             if(item.price >=1000){
+//               return (
 //               <div key={item.id}>
 //                 {item.title} - {item.price}
 //               </div>
 //             )
-//           })}
+//           }
+//           })
+//           }
 //         </div>
 //       </>
 //     )
-//   }
 // }
-
-
-// -************************-
-// function component
-const Component1 = (props) => {
-
-
-    let { arrJobs } = props;
-    return (
-      <>
-        <div className="job-lists">
-          {arrJobs.map((item, index) => {
-            return (
-              <div key={item.id}>
-                {item.title} - {item.price}
-              </div>
-            )
-          })
-          }
-        </div>
-      </>
-    )
-}
 
 export default Component1;
--- src/views/example/MyComponents.js
+++ src/views/example/MyComponents.js
@@ -5,9 +5,9 @@ class MyComponent extends React.Component {
     firstName: "",
     lastName: "",
     arrJobs :[
-        {id:'job1', title:'one1',price:'0990'},
-        {id:'job2', title:'one2',price:'0991'},
-        {id:'job3', title:'one3',price:'0992'}
+        {id:'job1', title:'one1',price:'1000'},
+        {id:'job2', title:'one2',price:'500'},
+        {id:'job3', title:'one3',price:'1500'}
     ]
   };
 
