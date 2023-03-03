e6eea61b9cc52e82c693f058c4beeaf48fdba998
Duc Minh <buiducminhnj13@gmail.com>
2023-03-03 22:00:19 +0700

    Stateless/StateFull Components

--- src/views/example/Component1.js
+++ src/views/example/Component1.js
@@ -1,31 +1,53 @@
 import React from "react";
-class Component1 extends React.Component {
-  render() {
-    console.log("checks props", this.props);
 
-    // cach 1
-    // let name = this.props.name;
-    // let age = this.props.age;
+// class component
+// class Component1 extends React.Component {
+//   render() {
+//     console.log("checks props", this.props);
 
-    // cach 2
-    let { name, age, address, arrJobs } = this.props;
-    let a = "";
+//     // cach 1
+//     // let name = this.props.name;
+//     // let age = this.props.age;
+
+//     // cach 2
+//     let { name, age, address, arrJobs } = this.props;
+//     return (
+//       <>
+//         <div className="job-lists">
+//           {arrJobs.map((item, index) => {
+//             return (
+//               <div key={item.id}>
+//                 {item.title} - {item.price}
+//               </div>
+//             )
+//           })}
+//         </div>
+//       </>
+//     )
+//   }
+// }
+
+
+// -************************-
+// function component
+const Component1 = (props) => {
+
+
+    let { arrJobs } = props;
     return (
       <>
         <div className="job-lists">
-          {
-            (a = arrJobs.map((item, index) => {
-              return (
-                <div key={item.id}>
-                  {item.title} - {item.price}
-                </div>
-              );
-            }))
+          {arrJobs.map((item, index) => {
+            return (
+              <div key={item.id}>
+                {item.title} - {item.price}
+              </div>
+            )
+          })
           }
-          {console.log('check map arr',a)}
         </div>
       </>
-    );
-  }
+    )
 }
+
 export default Component1;
