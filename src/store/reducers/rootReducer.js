3190989b68903f9b6d4c1f38a63a9aceaf28f148
Duc Minh <buiducminhnj13@gmail.com>
2023-03-24 17:24:54 +0700

    Setup Redux vào dự án React

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    user:[
+        {id: 1, name: 'minh'},
+        {id: 2, name: 'duc'},
+    ]
+}
+const rootReducer = (state = initState, action) => {
+    return state;
+}
+
+export default rootReducer;
