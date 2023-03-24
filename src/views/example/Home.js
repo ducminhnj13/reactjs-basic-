a2b25ab128c7abc2a71282f463157cc32251fdb5
Duc Minh <buiducminhnj13@gmail.com>
2023-03-24 17:44:30 +0700

    Mapping State (Redux) to Props (React)

--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router";
-import Color from '../HOC/Color';
-import logo from '../../assets/images/download.jpg'
+import Color from "../HOC/Color";
+import logo from "../../assets/images/download.jpg";
+import { connect } from "react-redux";
 
-class Home extends React.Component { 
-    componentDidMount(){
-        // setTimeout(() =>
-        // {
-        //     this.props.history.push('./todo')
-        // },3000)
-    }
-    render() {
-        return(
-            <>
-                <div>Hello Home Page</div>
-                <div>
-                <img src={logo} />
-                </div>
-            </>
-        )
-    }
+class Home extends React.Component {
+  componentDidMount() {
+    // setTimeout(() =>
+    // {
+    //     this.props.history.push('./todo')
+    // },3000)
+  }
+  render() {
+    console.log('check prop',this.props)
+    return (
+      <>
+        <div>Hello Home Page</div>
+        <div>
+          <img src={logo} />
+        </div>
+      </>
+    );
+  }
 }
 
 // export default withRouter(Home);
-export default Color(Home);
+const mapStateToProps = (state) => {
+  return { dataRedux: state.user };
+};
+
+export default connect(mapStateToProps)(Color(Home));
